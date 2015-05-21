a5d131afcab283b036e6420a40f7cb48e558961c
Tasfir Hossain Suman <tasfir_suman@yahoo.com>
2015-05-21 17:52:58 +0600

    add CKeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  mount Ckeditor::Engine => '/ckeditor'
   resources :articles
+
+
+
+  root to: "articles#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
